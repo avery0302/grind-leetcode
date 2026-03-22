0e56fd9452330a46db673f427272dcee9046cba5
chia <yuki2nenu@gmail.com>
2026-03-22 16:57:49 +0800

    4 优化语法

--- leetcode4.js
+++ leetcode4.js
@@ -45,21 +45,21 @@ var findMedianSortedArrays = function (nums1, nums2) {
 力扣第4题:寻找两个有序数组的中位数
 the 4th problem on leetcode: median of two sorted arrays
 思路:如果不考虑时间复杂度,最简单的方法就是合并,排序,取值
-approach: if we dont consider the time complexity, the easiest way is to merge, sort and get the value
+approach: if we don't consider time complexity, the easiest way is to merge, sort, and get the value
 但是排序函数时间复杂度过高,而给的又是两个有序数组
-but the sort function has a high time complexity, while we've already had two sorted arrays
+but the sorting function has a high time complexity, while we already have two sorted arrays
 所以直接两个指针,从两个数组中从小到大拿值
-so we create two pointers to get the value from the two arrays by order
+so we use two pointers to get values from the two arrays in order
 代码:数组个数为奇数和偶数时中位数计算方式不同
-code: the calculations of the median is different when the mount of the array is odd or even
+code: the calculation of the median is different when the length of the array is odd or even
 奇数只需要一个中间元素,偶数需要两个
-odd array has one middle element, while even array has two middle elements
+an odd-length array has one middle element, while an even-length array has two
 遍历次数是数组一半的长度
-the times of iteration is half of length of the array
+the number of iteration is half of the length of the array
 每次遍历要找到两个数组中较小的数值并移动指针
-find the smaller value of the two arrays and move the pointers every iteration
+find the smaller value from the two arrays and move the pointers in each iteration
 奇数直接返回中间的值,偶数返回中间两个值的平均值
-odd condition returns the middle element, while even condition returns the average of the two middle elements
+the odd case returns the middle element, while the even case returns the average of two middle elements
 所以这就是全部代码
 so this is the complete code
 看一下结果
