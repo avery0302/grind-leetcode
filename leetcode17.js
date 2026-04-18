8c23b59deff950140373ff0a72630b934d026a91
chia <yuki2nenu@gmail.com>
2026-04-18 17:04:22 +0800

    17 english script update

--- leetcode17.js
+++ leetcode17.js
@@ -36,13 +36,13 @@ var letterCombinations = function (digits) {
  * 思路: 本题提供了数字到字母的一对多映射,给定一串数字,要找到所有可能的字母映射
  * approach: this problem provides a mapping from digits to letters, and requires finding all possible letter combinations from the given string
  * 由于数字长度最大为4,所以首先想到四重循环逐层拼接,能够覆盖所有情况
- * because the maximum length of the string is 4, so the first idea is to use four nested loops to build combinations, this implementation can cover all possible cases
+ * the maximum length of the string is 4, so the first idea is to use four nested loops to build combinations, this approach can cover all possible cases
  * 为了精简代码,四重循环可以用递归来代替
- * to simplify the code, we can use recursive function to replace the four nested loops
+ * to simplify the code, we can use a recursive function to replace the four nested loops
  * 代码: 先初始化一个结果数组
  * code: first initialize a result array
  * 参考图片写出数字到字母的映射
- * create the mapping from digits to letters based on the picture in the problem
+ * create the mapping from digits to letters based on the problem description
  * 然后构建递归的基本结构
  * then build the basic structure of a recursive function
  * 增加递归的分支
