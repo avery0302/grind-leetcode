885cc3a2bdb9ab1f687c789a28afc9ca9deffdfe
chia <yuki2nenu@gmail.com>
2026-04-18 14:04:18 +0800

    17 done

--- leetcode17.js
+++ leetcode17.js
@@ -6,31 +6,17 @@ var letterCombinations = function (digits) {
     let res = []
     let len = digits.length
     let mapping = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
-    let comb = ''
-    for (const letter1 of mapping[digits[0]]) {
-        comb = letter1
-        if (len > 1) {
-            for (const letter2 of mapping[digits[1]]) {
-                comb = `${letter1}${letter2}`;
-                if (len > 2) {
-                    for (const letter3 of mapping[digits[2]]) {
-                        comb = `${letter1}${letter2}${letter3}`;
-                        if (len > 3) {
-                            for (const letter4 of mapping[digits[3]]) {
-                                comb = `${letter1}${letter2}${letter3}${letter4}`;
-                                res.push(comb)
-                            }
-                        } else {
-                            res.push(comb)
-                        }
-                    }
-                } else {
-                    res.push(comb)
-                }
-            }
-        } else {
+
+    function backtrack(idx, comb) {
+        if (idx === len) {
             res.push(comb)
+            return
+        }
+        for (const letter of mapping[digits[idx]]) {
+            backtrack(idx + 1, `${comb}${letter}`)
         }
     }
+
+    backtrack(0, '')
     return res
 };
