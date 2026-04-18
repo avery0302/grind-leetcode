6e2d2492ecce6ef446a006ab06d41b77b08df5c9
chia <yuki2nenu@gmail.com>
2026-04-18 16:05:24 +0800

    17 chinese script

--- leetcode17.js
+++ leetcode17.js
@@ -4,19 +4,43 @@
  */
 var letterCombinations = function (digits) {
     let res = []
+    let mapping = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz',
+    }
     let len = digits.length
-    let mapping = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
-    function backtrack(idx, comb) {
+    function trackback(idx, comb) {
         if (idx === len) {
             res.push(comb)
             return
         }
         for (const letter of mapping[digits[idx]]) {
-            backtrack(idx + 1, `${comb}${letter}`)
+            trackback(idx + 1, `${comb}${letter}`)
         }
     }
 
-    backtrack(0, '')
+    trackback(0, '')
     return res
 };
+
+/**
+ * 力扣17 电话号码的字母组合
+ * 思路: 本题提供了数字到字母的一对多映射,给定一串数字,要找到所有可能的字母映射
+ * 由于数字长度最大为4,所以首先想到四重循环逐层拼接,能够覆盖所有情况
+ * 为了精简代码,四重循环可以用递归来代替
+ * 代码: 先初始化一个结果数组
+ * 参考图片写出数字到字母的映射
+ * 然后构建递归的基本结构
+ * 增加递归的分支
+ * 最后加上递归的终止条件
+ * 所以这就是全部代码
+ * 看一下结果
+ * 完成
+ */
