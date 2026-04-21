cd17b05de512561cc43bca57bcf5be64b7fd159a
chia <1335850330@qq.com>
2026-04-21 17:27:17 +0800

    21 update

--- leetcode21.js
+++ leetcode21.js
@@ -12,27 +12,18 @@
  */
 var mergeTwoLists = function (list1, list2) {
     let dummy = new ListNode()
-    let list3 = dummy
-    let val1 = 0
-    let val2 = 0
-    while (list1 || list2) {
-        val1 = list1 ? list1.val : undefined
-        val2 = list2 ? list2.val : undefined
-        if (val2 === undefined) {
-            list3.next = list1
-            break
-        } else if (val1 === undefined) {
-            list3.next = list2
-            break
-        } else if (val1 <= val2) {
-            list3.next = new ListNode(val1)
+    let cur = dummy
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            cur.next = list1
             list1 = list1.next
-            list3 = list3.next
+            cur = cur.next
         } else {
-            list3.next = new ListNode(val2)
+            cur.next = list2
             list2 = list2.next
-            list3 = list3.next
+            cur = cur.next
         }
     }
+    cur.next = list1 || list2
     return dummy.next
 };
