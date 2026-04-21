c18208bcf9629e15440a9fe8a47a02cc9721e334
chia <1335850330@qq.com>
2026-04-21 14:45:56 +0800

    20 update

--- leetcode20.js
+++ leetcode20.js
@@ -5,13 +5,13 @@
 var isValid = function (s) {
     let stack = []
     let mapping = {')': '(', ']': '[', '}': '{'}
-    for (const char of s) {
-        if (!mapping[char] || stack.length === 0) {
-            stack.push(char)
-        } else if (mapping[char] === stack[stack.length - 1]) {
+    for (const c of s) {
+        if (!mapping[c] || stack.length === 0) {
+            stack.push(c)
+        } else if (mapping[c] === stack[stack.length - 1]) {
             stack.pop()
         } else {
-            stack.push(char)
+            stack.push(c)
         }
     }
     return !stack.length
