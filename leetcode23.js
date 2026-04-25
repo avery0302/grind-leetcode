68ef864dc6c64c6d209702bc2045e06d10f7ef31
chia <1335850330@qq.com>
2026-04-25 14:02:30 +0800

    23 update

--- leetcode23.js
+++ leetcode23.js
@@ -10,32 +10,38 @@
  * @return {ListNode}
  */
 var mergeKLists = function (lists) {
-    let dummy = new ListNode()
-    let cur = dummy
-    let len = lists.length
-    let minVal
-    let minIdx
-    let curVal
-    while (true) {
-        minVal = undefined
-        minIdx = undefined
-        for (let i = 0; i < len; i++) {
-            if (!lists[i]) {
-                continue
-            }
-            curVal = lists[i].val
-            if (minVal === undefined || curVal < minVal) {
-                minIdx = i
-                minVal = curVal
-            }
-        }
-        if (minVal === undefined) {
-            break
-        } else {
-            cur.next = lists[minIdx]
-            cur = cur.next
-            lists[minIdx] = lists[minIdx].next
-        }
+    let lists2 = []
+    if (lists.length === 0) {
+        return null
     }
-    return dummy.next
+
+    while (lists.length > 1) {
+        for (let i = 0; i < lists.length; i += 2) {
+            let l1 = lists[i]
+            let l2 = i + 1 < lists.length ? lists[i + 1] : null
+            let sum = merge2Lists(l1, l2)
+            lists2.push(sum)
+        }
+        lists = lists2
+        lists2 = []
+    }
+
+    return lists[lists.length - 1]
 };
+
+function merge2Lists(l1, l2) {
+    let node = new ListNode()
+    let cur = node
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            cur.next = l1
+            l1 = l1.next
+        } else {
+            cur.next = l2
+            l2 = l2.next
+        }
+        cur = cur.next
+    }
+    cur.next = l1 || l2
+    return node.next
+}
