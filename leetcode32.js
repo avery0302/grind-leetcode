d553b44a22a75c2830f4b467219b7c14fd91b032
chia <1335850330@qq.com>
2026-05-24 16:10:45 +0800

    32 update

--- leetcode32.js
+++ leetcode32.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var longestValidParentheses = function (s) {
-    let max = 0
     let len = s.length
     let stack = [-1]
+    let max = 0
     for (let i = 0; i < len; i++) {
         if (s[i] === '(') {
             stack.push(i)
-        } else if (stack.length !== 0 && s[stack[stack.length - 1]] === '(') {
-            stack.pop()
-            max = Math.max(max, i - stack[stack.length - 1])
         } else {
-            stack.push(i)
+            if (s[stack[stack.length - 1]] === '(') {
+                stack.pop()
+                max = Math.max(max, i - stack[stack.length - 1])
+            } else {
+                stack.push(i)
+            }
         }
     }
     return max
