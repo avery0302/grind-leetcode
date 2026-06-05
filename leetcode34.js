03ea7ebd97c49332899579607624c6a35d17d7c9
chia <1335850330@qq.com>
2026-06-05 11:02:29 +0800

    34

--- leetcode34.js
+++ leetcode34.js
@@ -9,58 +9,31 @@ var searchRange = function (nums, target) {
     if (len === 0 || target < nums[0] || target > nums[len - 1]) {
         return result
     }
-    let left = 0
-    let right = len - 1
-    let mid = -1
-    while (left <= right) {
-        mid = Math.floor((left + right) / 2)
-        if (nums[mid] === target) {
-            break
-        } else if (nums[mid] > target) {
-            right = mid - 1
-        } else if (nums[mid] < target) {
-            left = mid + 1
-        }
-    }
 
-    if (nums[mid] === target && nums[mid - 1] !== target) {
-        result[0] = mid
-    } else {
-        let left1 = left
-        let right1 = mid - 1
-        let mid1 = -1
-        while (left1 <= right1) {
-            mid1 = Math.floor((left1 + right1) / 2)
-            if (nums[mid1] === target && nums[mid1 - 1] !== target) {
-                result[0] = mid1
-                break
-            } else if (nums[mid1] === target && nums[mid1 - 1] === target) {
-                right1 = mid1 - 1
-            } else {
-                left1 = mid1 + 1
+    function findBoundry(dir) {
+        let left = 0
+        let right = len - 1
+        let mid = -1
+        let boundry = -1
+        while (left <= right) {
+            mid = Math.floor((left + right) / 2)
+            if (nums[mid] === target) {
+                boundry = mid
+                if (dir === 'left') {
+                    right = mid - 1
+                } else {
+                    left = mid + 1
+                }
+            } else if (nums[mid] > target) {
+                right = mid - 1
+            } else if (nums[mid] < target) {
+                left = mid + 1
             }
         }
+        return boundry
     }
 
-    if (nums[mid] === target && nums[mid + 1] !== target) {
-        result[1] = mid
-    } else {
-        let left2 = mid + 1
-        let right2 = right
-        let mid2 = -1
-        while (left2 <= right2) {
-            mid2 = Math.floor((left2 + right2) / 2)
-            if (nums[mid2] === target && nums[mid2 + 1] !== target) {
-                result[1] = mid2
-                break
-            } else if (nums[mid2] === target && nums[mid2 + 1] === target) {
-                left2 = mid2 + 1
-            } else {
-                right2 = mid2 - 1
-            }
-        }
-    }
+    result[0] = findBoundry('left')
+    result[1] = findBoundry('right')
     return result
 };
-
-searchRange([5, 7, 7, 8, 8, 10], 8)
