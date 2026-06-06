56502aa17288e84809dba17c92afa92e22b3ddea
chia <1335850330@qq.com>
2026-06-06 12:15:30 +0800

    34

--- leetcode34.js
+++ leetcode34.js
@@ -5,9 +5,8 @@
  */
 var searchRange = function (nums, target) {
     let len = nums.length
-    let result = [-1, -1]
     if (len === 0 || target < nums[0] || target > nums[len - 1]) {
-        return result
+        return [-1, -1]
     }
 
     function findBoundry(dir) {
@@ -17,23 +16,25 @@ var searchRange = function (nums, target) {
         let boundry = -1
         while (left <= right) {
             mid = Math.floor((left + right) / 2)
-            if (nums[mid] === target) {
+            if (nums[mid] < target) {
+                left = mid + 1
+            } else if (nums[mid] > target) {
+                right = mid - 1
+            } else if (nums[mid] === target) {
                 boundry = mid
                 if (dir === 'left') {
                     right = mid - 1
                 } else {
                     left = mid + 1
                 }
-            } else if (nums[mid] > target) {
-                right = mid - 1
-            } else if (nums[mid] < target) {
-                left = mid + 1
             }
         }
         return boundry
     }
 
-    result[0] = findBoundry('left')
-    result[1] = findBoundry('right')
-    return result
+    let leftBoundry = findBoundry('left')
+    let rightBoundry = findBoundry('right')
+
+    return [leftBoundry, rightBoundry]
+
 };
