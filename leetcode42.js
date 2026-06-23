4f7db996c219451ebc445c7d53753aa16699b8c6
chia <1335850330@qq.com>
2026-06-23 12:58:17 +0800

    42

--- leetcode42.js
+++ leetcode42.js
@@ -8,20 +8,21 @@ var trap = function (height) {
     let rightMax = Array(len).fill(0)
     let max = 0
     for (let i = 0; i < len - 1; i++) {
-        let h = height[i]
-        max = Math.max(max, h)
+        max = Math.max(max, height[i])
         leftMax[i + 1] = max
     }
     max = 0
     for (let i = len - 1; i > 0; i--) {
-        let h = height[i]
-        max = Math.max(max, h)
+        max = Math.max(max, height[i])
         rightMax[i - 1] = max
     }
     let sum = 0
     for (let i = 1; i < len - 1; i++) {
         let min = Math.min(leftMax[i], rightMax[i])
-        let cur = Math.max(min - height[i], 0)
+        let cur = min - height[i]
+        if (cur < 0) {
+            cur = 0
+        }
         sum += cur
     }
     return sum
